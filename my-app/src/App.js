24f074317ce51df6309127838d7dd8ae7ae22d6e
alanpaterson82 <alan_paterson82@hotmail.com>
2024-02-26 16:55:35 +0000

    initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import FunctionalGreeting from "./components/FunctionalGreeting";
+import FunctionalGreetingWithProps from "./components/FunctionalGreetingWithProps";
+
+function App() {
+  return (
+    <div className="App">
+      <FunctionalGreeting />
+      <FunctionalGreetingWithProps />
+    </div>
+  );
+}
+
+export default App;
