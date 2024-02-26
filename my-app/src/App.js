0c723baaf89b5c25e1c6c3ccb4a047669d4215b9
alanpaterson82 <alan_paterson82@hotmail.com>
2024-02-26 17:06:23 +0000

    add props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import FunctionalGreeting from "./components/FunctionalGreeting";
-import FunctionalGreetingWithProps from "./components/FunctionalGreetingWithProps";
+import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
 
 function App() {
   return (
     <div className="App">
-      <FunctionalGreeting />
-      <FunctionalGreetingWithProps />
+      <FunctionalGreetingWithProps greeting="Nice to meet you!" name="Mike" age="32"/>
     </div>
   );
 }
--- my-app/src/components/FunctionalGreetingWithProps.js
+++ my-app/src/components/FunctionalGreetingWithProps.js
@@ -1,5 +1,8 @@
 import React from "react";
 
-const FunctionalGreetingWithProps = () => <h1>Hello!</h1>;
+const FunctionalGreetingWithProps = (props) => {
+    console.log(props);
+    return <h1>Hello, {props.greeting}</h1>
+};
 
 export default FunctionalGreetingWithProps;
