3d8e96623b37a1fbded73c1ff7d0314b642348a0
alanpaterson82 <alan_paterson82@hotmail.com>
2024-03-04 17:02:51 +0000

    es7 snippets

---
+++ my-app/src/components/ES7Snippets.js
@@ -0,0 +1,25 @@
+// import React from 'react'
+
+// function ES7Snippets() {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// 
+
+// export default ES7Snippets
+
+// constructor(props) {
+//   super(props)
+
+//   this.state = {
+//      first
+//   }
+// }
+
+
+// console.log(first)
+
+
+// console.log('first', first)
