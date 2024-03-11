d1c2a35a6e7eded14dcc954124c315a802bbf914
alanpaterson82 <alan_paterson82@hotmail.com>
2024-03-11 10:04:40 +0000

    conditional rendering

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,19 @@ import "./App.css";
 import StatefulGreetingWithPrevState from "./components/StatefulGreetingWithPrevState";
 import EventsClass from "./components/EventsClass";
 import EventsFunctional from "./components/EventsFunctional";
+import EventBinding from "./components/EventBinding";
+import ConditionalRenderingFunctional from "./components/ConditionalRenderingFunctional";
+import ConditionalRenderingClass from "./components/ConditionalRenderingClass";
 
 function App() {
   return <div className="App">
       <StatefulGreetingWithPrevState />
       <EventsFunctional />
       <EventsClass/>
+      <EventBinding/>
+      <ConditionalRenderingClass/>
+      <ConditionalRenderingFunctional/>
+
     </div>;
 }
 
--- my-app/src/components/EventsClass.js
+++ my-app/src/components/EventsClass.js
@@ -1,16 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 
-export class EventsClass extends Component {
-  clickHandler() {
-    console.log("Clicked the class button");
-  }
-  render() {
-    return (
-      <div>
-        <button onClick={this.clickHandler}>click me - class component</button>
-      </div>
-    );
-  }
+class EventsClass extends React.Component {
+    clickHandler() {
+        console.log('Clicked the class button');
+    }
+    render() {
+        return (
+            <div>
+                <button onClick={this.clickHandler}>
+                    Click me - class component
+                </button>
+            </div>
+        )
+    }
 }
 
 export default EventsClass;
