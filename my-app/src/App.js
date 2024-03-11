0c1b2095aac69b78fd5f11bcb665f0900a97e07c
alanpaterson82 <alan_paterson82@hotmail.com>
2024-03-11 12:55:57 +0000

    adding form data

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,6 +11,7 @@ import RenderingLists from "./components/RenderingLists";
 import LifeCyclesCDM from "./components/LifeCyclesCDM";
 import LifeCyclesCDU from "./components/LifeCyclesCDU";
 import LifeCyclesCWU from "./components/LifeCyclesCWU";
+import ControlledForm from "./components/ControlledForm";
 
 function App() {
   return <div className="App">
@@ -26,6 +27,7 @@ function App() {
       <LifeCyclesCDM/>
       <LifeCyclesCDU/>
       <LifeCyclesCWU/>
+      <ControlledForm/>
 
     </div>;
 }
