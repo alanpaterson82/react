2303fab305530399c971afbbb334068287cc8d78
alanpaterson82 <alan_paterson82@hotmail.com>
2024-03-11 08:40:42 +0000

    handling events tutorial

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import StatefulGreetingWithPrevState from "./components/StatefulGreetingWithPrevState";
+import EventsClass from "./components/EventsClass";
+import EventsFunctional from "./components/EventsFunctional";
 
 function App() {
-  return (
-    <div className="App">
+  return <div className="App">
       <StatefulGreetingWithPrevState />
-    </div>
-  );
+      <EventsFunctional />
+      <EventsClass/>
+    </div>;
 }
 
 export default App;
